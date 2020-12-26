8e581027a0f3acab61a0c6c7a8d8213bfd64cbf7
Alex Hernandez <vlexhndz@gmail.com>
2020-12-26 20:31:25 +0100

    added username input on register

--- pages/register.js
+++ pages/register.js
@@ -106,7 +106,24 @@ const login = () => {
                   />
                 </div>
               </div>
-
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Username
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete="username"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
               <div>
                 <label
                   htmlFor="password"
