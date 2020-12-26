1788dcad0ca108cabb9aac4c7b312f63c57f0352
Alex Hernandez <vlexhndz@gmail.com>
2020-12-26 20:51:35 +0100

    mobile headers functionallity added

--- components/Layout/Dashboard/Header.js
+++ components/Layout/Dashboard/Header.js
@@ -303,30 +303,50 @@ const Header = () => {
         >
           <div className="pt-2 pb-3 space-y-1">
             {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
-            <a
-              href="#"
-              className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-            >
-              Dashboard
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-            >
-              Team
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-            >
-              Projects
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-            >
-              Calendar
-            </a>
+            <Link href="/dashboard">
+              <a
+                className={
+                  router.pathname === "/dashboard"
+                    ? "bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                }
+              >
+                Dashboard
+              </a>
+            </Link>
+            <Link href="/dashboard/orders">
+              <a
+                className={
+                  /\/order?/.test(router.pathname)
+                    ? "bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                }
+              >
+                Orders
+              </a>
+            </Link>
+            <Link href="/dashboard/messages">
+              <a
+                className={
+                  router.pathname === "/dashboard/messages"
+                    ? "bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                }
+              >
+                Messages
+              </a>
+            </Link>
+            <Link href="/dashboard/contacts">
+              <a
+                className={
+                  router.pathname === "/dashboard/contacts"
+                    ? "bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                }
+              >
+                Contacts
+              </a>
+            </Link>
           </div>
           <div className="pt-4 pb-3 border-t border-gray-200">
             <div className="flex items-center px-4 sm:px-6">
@@ -345,9 +365,8 @@ const Header = () => {
                   tom@example.com
                 </div>
               </div>
-              <button className="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              {/* <button className="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <span className="sr-only">View notifications</span>
-                {/* <!-- Heroicon name: bell --> */}
                 <svg
                   className="h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
@@ -363,27 +382,19 @@ const Header = () => {
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                   />
                 </svg>
-              </button>
+              </button> */}
             </div>
             <div className="mt-3 space-y-1">
-              <a
-                href="#"
-                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-              >
-                Your Profile
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-              >
-                Settings
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-              >
-                Sign out
-              </a>
+              <Link href="/dashboard/settings">
+                <a className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">
+                  Settings
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">
+                  Sign out
+                </a>
+              </Link>
             </div>
           </div>
         </div>
--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -233,29 +233,30 @@ const Header = ({ openTryItFree, handleCloseTryFree }) => {
             aria-labelledby="main-menu"
           >
             <div className="px-2 pt-2 pb-3" role="none">
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                role="menuitem"
-              >
-                Home
-              </a>
-
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                role="menuitem"
-              >
-                Features
-              </a>
-
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                role="menuitem"
-              >
-                Pricing
-              </a>
+              <Link href="/">
+                <a
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                  role="menuitem"
+                >
+                  Home
+                </a>
+              </Link>
+              <Link href="/features">
+                <a
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                  role="menuitem"
+                >
+                  Features
+                </a>
+              </Link>
+              <Link href="/pricing">
+                <a
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                  role="menuitem"
+                >
+                  Pricing
+                </a>
+              </Link>
             </div>
             <div role="none">
               <a
