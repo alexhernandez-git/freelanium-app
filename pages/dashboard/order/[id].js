14bc46b5bae311ada9b55b40eae3616ca10813f9
Alex Hernandez <vlexhndz@gmail.com>
2021-03-28 12:40:55 +0200

    solved order options 2

--- pages/dashboard/order/[id].js
+++ pages/dashboard/order/[id].js
@@ -329,7 +329,7 @@ const OrderBoard = () => {
                                   </span>
                                 ) : (
                                   <span
-                                    className="cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                     role="menuitem"
                                   >
                                     No options
